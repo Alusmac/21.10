14f1ccb3870e6fb3419a98f3bee2d86ee704932c
Alus <avp3085@gmail.com>
2025-11-07 11:37:18 +0100

    update macalus update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-print ("world")
-print("good morning")
-print("good morning")
-print("good morning")
-a = 4
-b = 5
-print("hello world",a,b)
+a = int(input("Vvedite chislo: "))
+b = input("Nazmite dezstvie +,-,/,*: ")
+c = int(input("Vvedite druge chislo chislo: "))
+
+if b == "+":
+   result = a + c
+elif b == "-":
+    result = a - c
+elif b == "/":
+    result = a / c
+elif b == "*":
+    result = a * c
+if result is not None:
+    print(result)
+
